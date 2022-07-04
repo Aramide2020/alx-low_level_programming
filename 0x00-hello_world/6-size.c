9834112d001ae5adb75c38d7b58eb62651482b12
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-04 05:19:45 -0700

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main- entry point
+ * main - entry point
  * Return: Always 0 (success)
  */
 int main(void)
@@ -8,8 +8,8 @@ int main(void)
 	int i;
 	long int l;
 	long long int ll;
-	float d
-	char c
+	float d;
+	char c;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
