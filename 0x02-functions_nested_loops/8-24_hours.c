df22a70d760ff4688811a5fd5e1685ddd388b9c3
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-06 18:32:38 -0700

    My first commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ void jack_bauer(void)
 
 	for (H = 0; H < 24; H++)
 	{
-		for ( M = 0; M < 60; M++)
+		for (M = 0; M < 60; M++)
 		{
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
@@ -23,4 +23,3 @@ void jack_bauer(void)
 		}
 	}
 }
-	   
