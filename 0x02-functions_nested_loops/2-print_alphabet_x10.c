c9ab1703a444adca8fdc2811b6621e7b277e3dd2
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-06 17:52:05 -0700

    My first commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-	chae i;
+	char i;
 	int j;
 
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
