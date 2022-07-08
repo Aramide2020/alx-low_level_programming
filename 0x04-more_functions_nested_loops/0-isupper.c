0458b298e5075297f9bea241ac3e2eb7927d56f9
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-08 13:34:15 -0700

    My first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,34 @@
-#include "main.h"
 #include <stdio.h>
+#include <ctype.h>
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
+int main () {
+   int var1 = 'M';
+   int var2 = 'm';
+   int var3 = '3';
 
-	    char c;
+		    
+   if( isupper(var1) ) {
+      printf("var1 = |%c| is uppercase character\n", var1 );
+   } else {
+      printf("var1 = |%c| is not uppercase character\n", var1 );
+   }
 
-	     c = 'A';
-	     printf("%c: %d\n", c, _isupper(c));
-	     c = 'a';
-	     printf("%c: %d\n", c, _isupper(c));
-	     return (0);
+   if( isupper(var2) ) {
+      printf("var2 = |%c| is uppercase character\n", var2 );
+   } else {
 
+       printf("var2 = |%c| is not uppercase character\n", var2 );
+
+   }   
+
+   if( isupper(var3) ) {
+
+   	printf("var3 = |%c| is uppercase character\n", var3 );
+
+   } else {
+ 	printf("var3 = |%c| is not uppercase character\n", var3 );
+
+   }			     
+
+   return(0);
 }
