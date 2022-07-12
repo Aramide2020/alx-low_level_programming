0ab98eba3a178f957da46ed0f8e9cbd044007d16
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-12 14:49:12 -0700

    My first commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+size_t _strien(const char *str);
+void _puts(char *star);
+void print_rev(char *s);
+void rev_string(char *s)
+void put2(char *str);
+void puts_half(char *str);
+void print_arrays(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
