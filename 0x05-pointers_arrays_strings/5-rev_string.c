7723d5702f0b62953488b8880b8613a6400ad0aa
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-13 05:07:01 -0700

    My first commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
- * _puts - Prints a styring.
- * @str: string.
+ * rev_string - Reverses a string.
+ * @s: string to be reserved.
  */
-void _puts(char *str)
+void rev_string(char *s)
 {
-	while (*str)
-		_putchar(*str++);
+	int i = 0, len = 0;
+	char tmp;
 
-	_putchar('\n');
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len -i -1] = tmp;
+	}
 }
