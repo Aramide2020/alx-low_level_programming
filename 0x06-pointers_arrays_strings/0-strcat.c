1eeef1ffce80fc550b78098de62b17e93fd218a5
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-14 13:45:30 -0700

    My first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - This function appends the src string to the dest string.
+ * @src: overwriting the terminating null byte
+ *
+ * Return: pointer to the resulting string.
+ */
+
+char *_strcat(char *dest, char *src);
+{
+
+	    char s1[98] = "Hello ";
+
+	        char s2[] = "World!\n";
+
+		    char *ptr;
+
+		   printf("Concatenated string: %s\n", strcat(destination source));
+
+		        return (0);
+
+}
