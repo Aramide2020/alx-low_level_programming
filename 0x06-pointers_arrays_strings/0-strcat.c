c56bbab5d753ee8b6a9b3f6596a56a2347d0b08c
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-14 14:00:56 -0700

    My first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,8 @@ char *_strcat(char *dest, char *src);
 	 char destination[] = "Hello ";
 	 char source[] = "World!";
 	 strcat(destination, source)
+
 	 printf("Concatenated String: %s\n", destination);
-         return 0;
+return (0);
 
 }
