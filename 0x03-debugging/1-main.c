8b1a69bb80020de76221700488f34d94a5d154f8
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-14 16:34:36 -0700

    My first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,11 +4,13 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 		int i;
 
 		printf("infinte loop incoming :(\n");
+
 		i = 0;	
 /*
 * 		while (i < 10)
@@ -16,6 +18,7 @@ int main(void)
 * 			putchar(i);
 * 		}
 */
-    		printf("infinite loop avoided! \\o/\n");
+    		printf("infinite loop avoided! \\o\/n");
+
 		return (0);
 }
