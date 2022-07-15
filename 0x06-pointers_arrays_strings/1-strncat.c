0336eac6c2bc488f6cd4be0e20508188070a7bfa
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-15 10:38:03 -0700

    My first commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- *_strncpy - copies n bytes of src to the dest string
- * @dest: string to copy to
- * @src: string being copied
- * @n: largest number of bytes to copy
+ *_strncat -appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ * @n: largest number of bytes to append
  *
  * Return: address of dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i; j;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
+	i = j = 0;
+	while (*(dest + i))
 		i++;
-	}
-	while (i < n)
+	while (i < n && *(src + j))
 	{
-		*(dest + i) = '\0';
+		*(dest + i) = *(src + j);
 		i++;
+		j++;
 	}
+	if (j < n)
+		*(dest + i) = *(src +j);
 	return (dest);
 }
