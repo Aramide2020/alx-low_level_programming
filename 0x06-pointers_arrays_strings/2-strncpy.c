ad755315817bbcdd5e11e77e6e99947eca122a85
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-15 11:52:00 -0700

    My first commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,6 @@
 #include "main.h"                                                                                                      
 /**                                                                           
- * _strncpy - copies n bytes of src to the dest string                           * @dest: string to copy to                                                                         
- * @src: string being copied                                                     * @n: largest number of bytes to copy                                           *
+ * _strncpy - copies n bytes of src to the dest string                           * @dest: string to copy to                                                      * @src: string being copied                                                     * @n: largest number of bytes to copy                                           *
  * Return: address of dest                                          
  */                                                                             char *_strncpy(char *dest, char *src, int n)                                   
 {
