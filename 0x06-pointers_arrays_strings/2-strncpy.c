8a191760667c8f92bcc2b3bafd3ee831a0e3c635
Aramide2020 <olakanmiyemi17@gmail.com>
2022-07-15 11:48:51 -0700

    My first commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,9 @@
-#include "main.h"                                                                                                                                                         
-
-/**                                                                                                                                                                       
- * _strncpy - copies n bytes of src to the dest string                                                                                        
- * @dest: string to copy to                                                                                                                         
- * @src: string being copied                                                                                                                                              
- * @n: largest number of bytes to copy                                                                                                                                    
- *
+#include "main.h"                                                                                                      
+/**                                                                           
+ * _strncpy - copies n bytes of src to the dest string                           * @dest: string to copy to                                                                         
+ * @src: string being copied                                                     * @n: largest number of bytes to copy                                           *
  * Return: address of dest                                          
- */                                                                                                                                                                       
-char *_strncpy(char *dest, char *src, int n)                                                                                                                              
+ */                                                                             char *_strncpy(char *dest, char *src, int n)                                   
 {
        	int i;
 
