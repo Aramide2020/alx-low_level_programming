32cc743caaa6e28cc46e56f28eea84bca14050fb
Aramide2020 <olakanmiyemi17@gmail.com>
2022-08-15 15:48:32 -0700

    my new commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 /**
  * listint_len - returns the number of elements in a linked listint_t list.
  * @h: pointer to the linked list
- * 
+ *
  *Return: number of elements in a linked listint_t list
  */
 size_t listint_len(const listint_t *h)
